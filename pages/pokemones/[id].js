fc808e847c64057aa223a19750a5d31e68971ad7
nleonn <nikoleonn@outlook.com>
2022-01-21 11:54:51 -0300

    commit inicial

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const Pokemon = ({ data }) => {
+  
+  return(
+    <div>
+      <h1>{data.name} numero {data.id}</h1>
+      <Image src={data.sprites.front_default} width={200} height={200} />
+      <Link href="/" >Volver al inicio</Link>
+    </div>
+  )
+};
+
+export default Pokemon
+
+export const getStaticProps = async ({ params }) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+  const data = await response.json()
+
+  return {
+    props: { data }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const paths = [
+    { params: { id: '1'} }
+  ]
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+//export const getServerSideProps = async ({ params }) => {
+//  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//  const data = await response.json()
+//
+//  return {
+//    props: { data }
+//  }
+//}
